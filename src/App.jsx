93f742f82c73433387ccf84fc2b49014c07c6a18
Lucas Menon <lucasguilarduccimenon@gmail.com>
2024-09-18 11:43:05 -0300

    Creating about me and contact area, i did not choose a good pic yet, but the area is done and working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styles from './App.module.css';
+import DevChar from './components/DevChar/DevChar';
 import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className={styles.App}>
       <Navbar />
+      <DevChar />
     </div>
   )
 }
---
+++ src/components/DevChar/DevChar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./DevChar.module.css";
+import { getImageUrl } from "../../utils";
+
+export const DevChar = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Olá, me chamo Lucas!</h1>
+        <p className={styles.description}>
+        Sou estudante de Análise e Desenvolvimento de Sistemas na Faculdade Vianna Júnior, com foco em desenvolvimento fullstack. Confira meu portifólio para saber mais sobre meus conhecimentos, projetos pessoais e experiências profissionais!
+        </p>
+        <a href="mailto:lucasguilarduccimenon@gmail.com" className={styles.contactBtn}>Entre em contato</a>
+      </div>
+      <img className={styles.imgEdit}src={getImageUrl("myPics/pfp2.jpeg")}/>
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
+    </section>
+  );
+};
+
+export default DevChar;
