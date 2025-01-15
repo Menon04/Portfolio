17bfa6259fab0f0f9fd026cc274d804601c213c8
Lucas Menon <lucasguilarduccimenon@gmail.com>
2025-01-15 20:50:38 -0300

    add: adicionado novos projetos

--- src/components/DevChar/DevChar.jsx
+++ src/components/DevChar/DevChar.jsx
@@ -10,7 +10,7 @@ const DevChar = () => {
         <p className={styles.description}>
         Sou estudante de Análise e Desenvolvimento de Sistemas na Faculdade Vianna Júnior, com foco em desenvolvimento fullstack. Confira meu portifólio para saber mais sobre meus conhecimentos, projetos pessoais e experiências profissionais!
         </p>
-        <a href="https://drive.google.com/uc?export=download&id=1BTZLqU25_fJ7zc_7x0XA0eEdotSjEckD9C05Kdkuksg" className={styles.contactBtn}>Currículo</a>
+        <a href="https://docs.google.com/document/d/1BTZLqU25_fJ7zc_7x0XA0eEdotSjEckD9C05Kdkuksg/edit?usp=sharing" className={styles.contactBtn}>Currículo</a>
       </div>
       <img className={styles.imgEdit}src={getImageUrl("myPics/pfp01.png")}/>
         <div className={styles.topBlur} />
