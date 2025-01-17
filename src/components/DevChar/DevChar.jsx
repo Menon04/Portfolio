3ab84975dfb0aa52e6b246d17c378f331595002e
Lucas Menon <lucasguilarduccimenon@gmail.com>
2025-01-17 02:38:01 -0300

    fix: algumas incoerencias no texto

--- src/components/DevChar/DevChar.jsx
+++ src/components/DevChar/DevChar.jsx
@@ -8,7 +8,7 @@ const DevChar = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Olá, me chamo Lucas!</h1>
         <p className={styles.description}>
-        Sou estudante de Análise e Desenvolvimento de Sistemas na Faculdade Vianna Júnior, com foco em desenvolvimento fullstack. Confira meu portifólio para saber mais sobre meus conhecimentos, projetos pessoais e experiências profissionais!
+        Sou estudante de Análise e Desenvolvimento de Sistemas na Faculdade Vianna Júnior, com foco em desenvolvimento fullstack. Confira meu portfólio para saber mais sobre meus conhecimentos, projetos pessoais e experiências profissionais!
         </p>
         <a href="https://docs.google.com/document/d/1BTZLqU25_fJ7zc_7x0XA0eEdotSjEckD9C05Kdkuksg/edit?usp=sharing" className={styles.contactBtn}>Currículo</a>
       </div>
