685d10572580b56432fc5b0ecc174a5f40da38d6
Lucas Menon <lucasguilarduccimenon@gmail.com>
2025-01-17 02:36:11 -0300

    add: titulo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/">Portifólio</a>
+      <a className={styles.title} href="/">Portfólio</a>
       <div className={styles.menu}>
         <img
           className={styles.menuBtn}
